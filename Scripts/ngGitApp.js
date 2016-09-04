5cdd7fd90a155687a9cf7de31dbb1bcc09cf9339
vishalsumant <vishal.sumant@gmail.com>
2016-09-04 18:37:55 -0400

    Angular JS Git Hub API
    Using GitHub’s public API, make a request to pull back valid
    repositories for the entered user.

---
+++ Scripts/ngGitApp.js
@@ -0,0 +1,61 @@
+﻿var gitApp = angular.module('GitApp', ['ngRoute']);
+
+gitApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/UserInfo', {
+        templateUrl: 'views/UserInfo.htm',
+        controller: 'GitHubCtrl'
+    }).when('/Repositories', {
+        templateUrl: 'views/Repositories.htm',
+        controller: 'GitHubCtrl'
+    })
+} ]);
+
+var GitHubCtrl = gitApp.controller('GitHubCtrl', function ($scope, $http, $location) {
+    $scope.SearchUser = false;
+    $scope.SearchRepos = false;
+
+    $scope.getGitInfo = function () {
+        $scope.userNotFound = false;
+        $scope.loaded = false;
+
+        if ($scope.userName == "undefined") $scope.userName = "";
+
+        $http.get("https://api.github.com/users/" + $scope.userName)
+               .success(function (data) {
+                   if (data.name == "") data.name = data.login;
+                   $scope.user = data;
+                   $scope.loaded = true;
+               })
+               .error(function () {
+                   $scope.userNotFound = true;
+               });
+    }
+
+    $scope.getGitRepos = function () {
+        $scope.reposFound = false;
+        alert($scope.userName);
+        if ($scope.userName == "undefined") $scope.userName = "";
+
+        $http.get("https://api.github.com/users/" + $scope.userName + "/repos").success(function (data) {
+            $scope.repos = data;
+            $scope.reposFound = data.length > 0;
+        });
+    }
+
+
+    $scope.UserSearch = function () {
+        $scope.userName = "";
+        $scope.SearchUser = true;
+        $scope.SearchRepos = false;
+    }
+
+    $scope.ReposSearch = function () {
+        $scope.userName = "";
+        $scope.SearchUser = true;
+        $scope.SearchRepos = true;
+    }
+});
+
+
+
+
